2ded43c1f37a8b5cd625af623dfce33b43002a39
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-17 10:35:31 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sobre.css">
+    <title>Sobre Nós</title>
+</head>
+<body bgcolor="black">
+    <div>
+    <h1>Sobre nós</h1>
+    <h4>Focados em lhes trazer sempre o melhor serviço possível</h4>
+    <h1>Assistência equipada com ferramentas para tornar a manutenção o mais perfeito possível</h1>
+    <img src="imggpu.jpg.webp" alt="processador2">      <img src="bancada.jpg" alt="bancada tecnico">
+    <h1>Ferramentas de última geração</h1>
+    <img src="microscopio" alt="microscopio"> <img src="estacaosolda.jpg" alt="estação de solda"> <img src="gpu.jpg" alt="processador">
+    </div>
+    <div>
+        <ul>
+            <li><a href="index.php"> Home</a><br></li>
+            <li> <a href="login.php" target = "blank">Faça login</a></li>
+            <li><a href="cadastro.php" target = "blank">Faça seu cadastro</a></li>        
+        </ul>
+    </div>
+    
+</body>
+</html>
