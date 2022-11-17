9c900aab2c8ebe894d9d9955a82e3cdee8b7a9f2
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-17 10:33:59 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Home ZTECH</title>
+</head>
+<body bgcolor="black">
+    <img src="logo2.jpg" alt="logomarca empresa">
+   <div class = "txt"><h1>Ztech Assistência Técnica</h1> <h3>A assistência que precisa esta aqui! <p></p> </h3>
+   <h3> Atuamos na área de consertos em <b>Notebooks,</b> <b>Impressoras,</b> <b>Celulares</b> e <b>Televisores</b></h3>
+   <img id="cel" src="celular.jpeg" alt="celular">
+   <img  id="comp" src="computador.jpeg" alt="computador">
+   <img id="molhou" src="molhou.jpeg" alt="celmolhado">
+    </div>
+</div>
+    <div>
+    <ul id="menux">
+        <nav id="menu">
+        <li> <a href="sobre.php" target = "blank"><ion-icon name="information-outline"></ion-icon>  Quem somos</a></li>
+        <li> <a href="login.php" target = "blank"><ion-icon name="person-outline"></ion-icon>  Faça login</a></li>
+        <li> <a href="cadastro.php" target = "blank"><ion-icon name="contract-outline"></ion-icon> Cadastro</a></li>        
+        <br><br>
+    </ul>
+    </nav>
+    </div>
+   
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
