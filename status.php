833f6febe2b896ee8f6c5571431ec4a58bc92b16
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-17 10:35:53 -0300

    Create status.php

---
+++ status.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="status.css">
+    <title>Status</title>
+</head>
+<body >
+    <h1>Status produto</h1>
+    <?php 
+    include conecta.php;
+    $valida
+    ?>
+    <div class="container">
+        <div class="box" style="--clr:#89ec5b;" >
+        <div class="content">
+            <div class="icon"><ion-icon name="ellipsis-horizontal-circle"></ion-icon></div>
+            <div class="text"></div>
+            <h3>About</h3>
+            <p>Sobre nos Produtos atendidos</p>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+    <div class="box" style="--clr:#89ec5b;" >
+        <div class="content">
+            <div class="icon"><ion-icon name="storefront"></ion-icon></div>
+            <div class="text"></div>
+            <h3>Reotnar a home</h3>
+            <p>Voltar a tela principal</p>
+            <a href="index.php">Home</a>
+        </div>
+    </div>
+    <div class="box" style="--clr:#5b98eb;" >
+        <div class="content">
+            <div class="icon"><ion-icon name="build"></ion-icon></div>
+            <div class="text"></div>
+            <h3>Login</h3>
+            <p>Faça login</p>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <div class="container">
+        <div class="item">
+            <div class="circle">
+                <i class="bi bi-card-text"></i>
+                <h2>Serviço recebido</h2>
+            </div>
+        </div>
+    </div>
+    <h3>Deseja entrar em contato com o técnico responsável?</h3>
+    <a href="https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F5543991327778%2F&e=ATOwDL1PHlSDAhamZhUbq1pXqF99Ra0z9ndEgocuhl_Nlpdgt0ct0E4wzqEp_yapCtc-loIWV4xqJDz9Rp9bTKSTgHcy1g3vm8QeEYM&s=1"><ion-icon name="chatbubble-outline"></ion-icon>Acesse nosso Whatsapp</a>
+    
+</body>
+</html>
